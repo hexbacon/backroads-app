7e8c1eac9bab0058a0a0d9ef8d32daf60809a510
Christopher Mena <mena.christopher.student@gmail.com>
2023-04-06 22:15:13 -0400

    added missing href

--- src/components/About.js
+++ src/components/About.js
@@ -25,7 +25,7 @@ const About = () => {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="#" className="btn">read more</a>
+          <a href="#home" className="btn">read more</a>
         </article>
       </div>
     </section>
